93bbf6836babd0e8953bc783e7dd2534a8eff424
inan-normal <inanboshesap@gmail.com>
2025-09-21 18:06:52 +0300

    Update player.js

--- api/player.js
+++ api/player.js
@@ -1,6 +1,8 @@
 export default async function handler(req, res) {
-  const { tag } = req.query; // URL'den oyuncu tag'i al
-  const token = process.env.CLASH_API_TOKEN; // Vercel Environment Variable
+  const { tag } = req.query;
+
+  // Token burada direkt yazıldı
+  const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjBkMzc1MWIyLTExNGMtNDkxYi05ZGE4LTc4ODhjN2Q4Y2M5MiIsImlhdCI6MTc1ODQ2NTI4Niwic3ViIjoiZGV2ZWxvcGVyLzg1NThjY2ZlLWJkNTEtYmVjMC1mNTdmLWExNGU1MjMxZGU2OCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE4NS4yNy4xMzQuMTIzIl0sInR5cGUiOiJjbGllbnQifV19.cobvXHigdWzf7OR_O_PDI01DZ91JxlWy7GXuNtTcD9Sj4VOsX0etrJra5AyeaiQmQXuMQx07wfo_mC4iAHxHkw";
 
   const response = await fetch(
     `https://api.clashofclans.com/v1/players/${encodeURIComponent(tag)}`,

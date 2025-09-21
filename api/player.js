3babb30e24b1c0c0935ba85c32b323f66f97afa7
inan-normal <inanboshesap@gmail.com>
2025-09-21 18:01:24 +0300

    Create player.js

---
+++ api/player.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  const { tag } = req.query; // URL'den oyuncu tag'i alınıyor
+  const token = process.env.CLASH_API_TOKEN; // Vercel Environment Variable
+
+  const response = await fetch(
+    `https://api.clashofclans.com/v1/players/${encodeURIComponent(tag)}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    return res.status(response.status).json({ error: "API hatası" });
+  }
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
